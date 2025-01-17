021569a992de55e8728ec508203b303dfb6fb949
Droides1826 <jesus_dgonzalez@soy.sena.co>
2025-01-17 09:18:44 -0500

    Arreglo de calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -18,26 +18,26 @@ if __name__ == "__main__":
     calc = Calculadora()
     while True:
         print("Seleccione la operación:")
-        print("1. Sumar")
-        print("2. Restar")
-        print("3. Multiplicar")
-        print("4. Dividir")
-        print("5. Salir")
+        print("5. Sumar")
+        print("1. Restar")
+        print("4. Multiplicar")
+        print("2. Dividir")
+        print("3. Salir")
         opcion = input("Ingrese la opción (1/2/3/4/5): ")
 
-        if opcion == '5':
+        if opcion == '3':
             break
 
         num1 = float(input("Ingrese el primer numero: "))
         num2 = float(input("Ingrese el segundo numero: "))
 
-        if opcion == '1':
+        if opcion == '5':
             print(f"Resultado: {calc.sumar(num1, num2)}")
-        elif opcion == '2':
+        elif opcion == '1':
             print(f"Resultado: {calc.restar(num1, num2)}")
-        elif opcion == '3':
-            print(f"Resultado: {calc.multiplicar(num1, num2)}")
         elif opcion == '4':
+            print(f"Resultado: {calc.multiplicar(num1, num2)}")
+        elif opcion == '2':
             try:
                 print(f"Resultado: {calc.dividir(num1, num2)}")
             except ValueError as e:
